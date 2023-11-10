a2cbef5f2e994920d5dc91f8564d6d4023e0b5e4
Duncan Temple Lang <duncan@r-project.org>
2023-11-10 08:15:43 -0800

    can get counts across multiple years

--- R/contributions.R
+++ R/contributions.R
@@ -29,15 +29,57 @@ function(user, from = Sys.Date() - 365, to = Sys.Date())
 
     if(is.character(to))
         to = as.Date(to, "%Y-%m-%d")        
-    
+
+    dates = mkDateSeq(from, to)
+
+    if(length(dates) > 2) {
+        ans = do.call(rbind, mapply(function(a, b) getContributions(user, a, b),
+                                    dates[-length(dates)],
+                                    # have go from start to end of that year, not 1st day of next year
+                                    # so subtract 1 day.  But for the last date, need to leave it as is.
+                                    dates[-1] - c(rep(1, length(dates) - 2), 0),
+                                    SIMPLIFY = FALSE))
+        ans[ ans$date >= from & ans$date <= to, ]
+    } else
+        getContributions(user, from, to)
+}
+
+
+getContributions =
+function(user, from, to)        
+{
     range = format(c(from, to), "%Y-%m-%d")
+    
     html = getForm(sprintf("https://github.com/users/%s/contributions", user), from = range[1], to = range[2])
 
     doc = htmlParse(html)
     ans = parseContributions(doc)
 
-    ans[ ans$date <= to, ]
+    ans[ ans$date >= from & ans$date <= to, ]
 }
+
+mkDateSeq =
+    # Given a start and end date, create a sequence
+    # of mid points that are 1 year apart.
+function(a, b)
+{
+    if(format(a, "%m/%d") != "01/01") 
+        a = as.Date( paste0(format(a, "%Y"), "/1/1"), "%Y/%m/%d")
+    
+    sq = seq(a, b, by = "year") # 365)
+    if(sq[ length(sq) ] < b)
+        sq = c(sq, b)    
+
+    sq
+    
+#  if(b - a
+#  yr = format(c(a, b), "%Y%")
+#  browser()
+#  yr = unique(yr)
+#  if(length(yr)
+
+}
+
 
 parseContributions =
 function(doc)
@@ -45,7 +87,7 @@ function(doc)
     td = getNodeSet(doc, "//table//td[@data-date]")
 
     tmp = gsub(" .*", "", sapply(td, xmlValue))
-#browser()
+
     # count = as.integer(tmp) 
     # count[is.na(count)] = 0
     # gives a warning about NAs
